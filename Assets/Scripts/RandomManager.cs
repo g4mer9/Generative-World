bd3b94852779d4a208d51180171bde3ec9eb6c07
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-21 18:09:07 -0700

    finished

--- Assets/Scripts/RandomManager.cs
+++ Assets/Scripts/RandomManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RandomManager : MonoBehaviour
 {
     public GameObject itemToRandomize;
+    public GameObject[] toShuffle;
+    private Vector3 defaultPosition = new Vector3(0, 0, 0);
+    private Vector3[] shuffledPositions;
+    private int[] usedIndexes = {7, 8, 9, 10, 11, 12};
     public Material[] possibleMaterials;
     private static RandomManager instance;
     public static RandomManager Instance { get { return instance; } }
@@ -13,6 +18,7 @@ public class RandomManager : MonoBehaviour
     {
         if (instance != null && instance != this) { Destroy(this.gameObject); }
         else instance = this;
+        shuffledPositions = new Vector3[] { defaultPosition, defaultPosition, defaultPosition, defaultPosition, defaultPosition, defaultPosition };
         IEnumerator spawnTimer = SpawnTimer();
         StartCoroutine(spawnTimer);
     }
@@ -26,13 +32,39 @@ public class RandomManager : MonoBehaviour
             newObject.transform.localScale = new Vector3(Random.Range(0.5f, 5), Random.Range(0.5f, 5), Random.Range(0.5f, 5));
             newObject.GetComponent<Renderer>().material = possibleMaterials[Random.Range(0, possibleMaterials.Length)];
             newObject.SetActive(true);
-            Debug.Log("Spawned");
+            //Debug.Log("Spawned");
             yield return new WaitForSeconds(Random.Range(.5f, 1.5f));
         }
     }
 
     private void Update()
     {
+        //SHUFFLING CODE===========================================================================================================================================
+        if(Input.GetKeyDown(KeyCode.S) && toShuffle.Length > 0)
+        {
+            for (int i = 0; i < toShuffle.Length; i++)
+            {
+                shuffledPositions[i] = toShuffle[i].transform.position;
+            }
+            for (int i = 0; i < toShuffle.Length; i++)
+            {
+                int randomIndex = 7;//magic number
+                while (randomIndex ==7 || usedIndexes.Contains(randomIndex))
+                {
+                    randomIndex = Random.Range(0, toShuffle.Length);
+                }
+                usedIndexes.SetValue(randomIndex, i);
+                //Debug.Log(i.ToString() + " " + randomIndex.ToString());
+
+                toShuffle[i].transform.position = shuffledPositions[randomIndex];
+               
+            }
+
+            //reset usedIndexes to initial values
+            usedIndexes = new int[] { 7, 8, 9, 10, 11, 12 };
+        }
+        //POOLING CODE=============================================================================================================================================
+
         GameObject pooledCubes = ObjectPool.Instance.GetPooledObject();
         if (Input.GetKeyDown(KeyCode.Space) && pooledCubes != null)
         {
